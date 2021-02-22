9526decf07478aa181d8e10d50bca0d45af75736
DanielaLevezinho <daniela.levezinho@outlook.pt>
2021-02-22 19:10:23 +0000

    first live

---
+++ index.js
@@ -0,0 +1,22 @@
+const colyseus = require("colyseus");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const { ChessRoom } = require("./room");
+const port = process.env.port || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  // console.log('request')
+  res.send('GET request to the homepage')
+})
+
+const gameServer = new colyseus.Server({
+  server: http.createServer(app),
+});
+
+gameServer.listen(port);
+gameServer.define("room", ChessRoom);
