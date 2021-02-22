3eadc11fa4159c8393270d99496432504f5f2579
DanielaLevezinho <daniela.levezinho@outlook.pt>
2021-02-22 19:12:37 +0000

    first live

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const { ChessRoom } = require("./room");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
